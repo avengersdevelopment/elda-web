626af7d2a1e1a1e0822d2c5f713543e62f7a1a68
Rigen Maulana <rygen.dev@gmail.com>
2025-02-22 02:42:32 +0700

    fix: change stt on assist

--- src/app/sign-in/_components/container.tsx
+++ src/app/sign-in/_components/container.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { EyeIcon } from "lucide-react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 export default function Container() {
